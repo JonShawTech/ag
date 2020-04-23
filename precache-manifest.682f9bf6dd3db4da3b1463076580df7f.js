536cd959c7fd6b145222b80f6612f4fd16572f3e
Jon Shaw <jonshawtech@gmail.com>
2020-04-22 20:36:17 -0400

    Updates

---
+++ precache-manifest.682f9bf6dd3db4da3b1463076580df7f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44ccbe973c56e6ca8ff412bde73c0872",
+    "url": "/ag/index.html"
+  },
+  {
+    "revision": "c9d37bbb269892112a13",
+    "url": "/ag/static/css/main.10bcaa8e.chunk.css"
+  },
+  {
+    "revision": "167058c7960fc4ae8422",
+    "url": "/ag/static/js/2.414ae6e1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ag/static/js/2.414ae6e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9d37bbb269892112a13",
+    "url": "/ag/static/js/main.14aea446.chunk.js"
+  },
+  {
+    "revision": "f0162eed6aef7db45db4",
+    "url": "/ag/static/js/runtime-main.7927d828.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/ag/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/ag/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/ag/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/ag/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/ag/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
